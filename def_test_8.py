98ec808b32e9114f79a12681d1191d742a5feff2
rrayddev <rrayddev@gmail.com>
2026-06-05 10:27:11 +0700

    Delete def_test_8.py

--- def_test_8.py
+++
@@ -1,8 +0,0 @@
-def estimate_arrival(distance_km, weather_condition):
-    travel_time = distance_km * 3
-    if (weather_condition == "rainy"):
-        travel_time += 10
-    return travel_time
-distance_km = 5
-weather_condition = "rainy"
-print(estimate_arrival(distance_km, weather_condition), "Minutes")
