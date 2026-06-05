0a955e5cefe9daf2b55386f5aeb15dc8ef5edf1b
rrayddev <rrayddev@gmail.com>
2026-06-05 10:27:23 +0700

    Delete def_test_9.py

--- def_test_9.py
+++
@@ -1,7 +0,0 @@
-def validate_tweet(input_text):
-        if len(input_text) <= 140:
-            return input_text
-        else:
-            return input_text[0:140] + "..."
-input_text = input("Masukan caption: ")
-print(validate_tweet(input_text))
